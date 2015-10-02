42733d9d575df979050aba1e1051e26267cd116a
Kennedy Oliveira <kennedy.oliveira@outlook.com>
2015-10-02 11:45:27 -0300

    new functions

--- main.rb
+++ main.rb
@@ -2,6 +2,21 @@ require 'sinatra'
 require 'net/http'
 require 'json'
 
+def send_request(uri, params=nil)
+  uri = URI.parse(uri)
+
+  # Extra params for custom badges
+  uri.query = URI.encode_www_form(params) unless params.nil? or params.empty?
+
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+  request = Net::HTTP::Get.new(uri.request_uri)
+
+  http.request(request)
+end
+
 #
 # Generate the Badge in SVG format.
 #
@@ -11,17 +26,9 @@ require 'json'
 # @param [Hash] params Extra params for customizing the badge // TODO Document this crap too
 # @return [String] SVG for the badge
 def build_badge(text, status, color, params=nil)
-  uri = URI.parse("https://img.shields.io/badge/#{URI.encode(text.to_s)}-#{URI.encode(status.to_s)}-#{color}.svg")
+  uri = "https://img.shields.io/badge/#{URI.encode(text.to_s)}-#{URI.encode(status.to_s)}-#{color}.svg"
 
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-  # TODO set the params for extra options
-
-  request = Net::HTTP::Get.new(uri.request_uri)
-
-  resp = http.request(request)
+  resp = send_request(uri, params)
 
   raise 'Error generating the badge' unless resp.code == '200'
 
@@ -29,17 +36,17 @@ def build_badge(text, status, color, params=nil)
 end
 
 # GitHub API URL
-GITHUB_API = 'https://api.github.com/repos/'
+GITHUB_API = 'https://api.github.com/repos'
 
 #
 # Generate badges with download count
 # Can be by tag, for the latest, summing all assets download or by a specified file name
 #
-get '/downloadcount/:user/:repo/:tag/:asset' do
+get '/downloadcount/:user/:repo/:tag/:asset.svg' do
   text = params['text'] ||= 'downloads'
   color = params['color'] ||= 'brightgreen'
 
-  git_uri = URI.parse("#{GITHUB_API}/#{params['user']}/#{params['repo']}/releases/")
+  git_uri = "#{GITHUB_API}/#{params['user']}/#{params['repo']}/releases/"
 
   if params['tag'] == 'latest'
     git_uri += "#{params['tag']}"
@@ -47,26 +54,19 @@ get '/downloadcount/:user/:repo/:tag/:asset' do
     git_uri += "tags/#{params['tag']}"
   end
 
-  client = Net::HTTP.new(git_uri.host, git_uri.port)
-  client.use_ssl =true
-  client.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-  git_request = Net::HTTP::Get.new(git_uri.request_uri)
-
-  git_resp = client.request(git_request)
+  git_resp = send_request(git_uri)
 
   if git_resp.code != '200'
     status = 'vendor error'
     text = 'vendor'
     color = 'lightgray'
   else
-
     resp_json = JSON.parse(git_resp.body)
 
     if params['asset'] == 'total'
-      status = resp_json['assets'].inject(0) { |total, asset| total + asset['download_count']}
+      status = resp_json['assets'].inject(0) { |total, asset| total + asset['download_count'] }
     else
-      asset = resp_json['assets'].find {|asset| asset['name'] == params['asset']}
+      asset = resp_json['assets'].find { |asset| asset['name'] == params['asset'] }
 
       if asset.nil?
         text = 'vendor'
@@ -78,10 +78,87 @@ get '/downloadcount/:user/:repo/:tag/:asset' do
     end
   end
 
-  begin
-    badge = build_badge(text, status, color)
-    response.header['Content-Type'] = 'image/svg+xml;charset=utf-8'
-    response.header['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-    response.body = badge
+  badge = build_badge(text, status, color)
+  response.header['Content-Type'] = 'image/svg+xml;charset=utf-8'
+  response.header['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+  response.body = badge
+end
+
+#
+# Get the last release tag name
+#
+get '/release/:user/:repo.svg' do
+  text = params['text'] ||= 'release'
+  color = params['color'] ||= 'brightgreen'
+
+  git_uri = "#{GITHUB_API}/#{params['user']}/#{params['repo']}/releases/latest"
+
+  resp = send_request(git_uri)
+
+  if resp.code != '200'
+    text = 'vendor'
+    status = 'vendor error'
+    color = 'lightgray'
+  else
+    status = JSON.parse(resp.body)['tag_name']
   end
+
+  badge = build_badge(text, status, color)
+  response.header['Content-Type'] = 'image/svg+xml;charset=utf-8'
+  response.header['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+  response.body = badge
+end
+
+#
+# Get the issues open
+#
+get '/issues/:user/:repo.svg' do
+  text = params['text'] ||= 'open--issues'
+  color = params['color'] ||= 'red'
+
+  git_uri = "#{GITHUB_API}/#{params['user']}/#{params['repo']}"
+
+  resp = send_request(git_uri)
+
+  if resp.code != '200'
+    text = 'vendor'
+    status = 'vendor error'
+    color = 'lightgray'
+  else
+    status = JSON.parse(resp.body)['open_issues_count']
+  end
+
+  badge = build_badge(text, status, color)
+  response.header['Content-Type'] = 'image/svg+xml;charset=utf-8'
+  response.header['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+  response.body = badge
+end
+
+#
+# Get the total commits
+#
+get '/commits/:user/:repo.svg' do
+  # https://api.github.com/repos/kennedyoliveira/alfred-rates/stats/commit_activity
+  text = params['text'] ||= 'commits'
+  color = params['color'] ||= 'blue'
+
+  git_uri = "#{GITHUB_API}/#{params['user']}/#{params['repo']}/contributors"
+
+  resp = send_request(git_uri)
+
+  if resp.code != '200'
+    text = 'vendor'
+    status = 'vendor error'
+    color = 'lightgray'
+  else
+    contributors = JSON.parse(resp.body)
+    total_commits = contributors.inject(0) {|sum, contributor| sum + contributor['contributions']}
+
+    status = total_commits
+  end
+
+  badge = build_badge(text, status, color)
+  response.header['Content-Type'] = 'image/svg+xml;charset=utf-8'
+  response.header['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+  response.body = badge
 end
